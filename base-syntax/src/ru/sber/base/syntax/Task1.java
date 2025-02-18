16740a00b896e04f7780c94700d5349b4b35a34d
hearwy <mipwqre@gmail.com>
2025-02-18 09:52:16 +0300

    Commit Message

--- base-syntax/src/ru/sber/base/syntax/Task1.java
+++ base-syntax/src/ru/sber/base/syntax/Task1.java
@@ -1,4 +1,11 @@
 package ru.sber.base.syntax;
-
+import java.util.Scanner;
 public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите Ваше имя: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
+    }
 }
