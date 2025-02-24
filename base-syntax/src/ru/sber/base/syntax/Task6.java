0d14bd34f17747e3bfe0e438584d33ad8aae37d3
hearwy <mipwqre@gmail.com>
2025-02-24 21:43:46 +0300

    Commit Message

--- base-syntax/src/ru/sber/base/syntax/Task6.java
+++ base-syntax/src/ru/sber/base/syntax/Task6.java
@@ -5,22 +5,19 @@ import java.util.Arrays;
 public class Task6 {
     public static void main(String[] args) {
         int[] arr = new int[100];
-        for (int i = 0; i < arr.length; ++i) {
-            arr[i] = (int) (Math.random() * 1000);
-        }
-        System.out.println("Массив " + Arrays.toString(arr));
-        int maxValue = arr[0];
+        int maxValue = 0;
         int maxIndex = 0;
-        double sum = 0;
-        for (int i = 1; i < arr.length; i++) {
+        float sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 1000);
             if (arr[i] > maxValue) {
                 maxValue = arr[i];
-                maxIndex = i;
-                sum = sum + arr[i];
+                maxIndex = i + 1;
             }
+            sum += arr[i];
         }
-        double midsum = sum / arr.length;
+        System.out.println("Массив " + Arrays.toString(arr));
         System.out.println("Максимальное значение = [" + maxValue + "] находится в элементе под индексом [" + maxIndex + "]");
-        System.out.println("Среднее значение = [" + midsum + "]");
+        System.out.println("Среднее значение = [" + (sum / arr.length) + "]");
     }
 }
