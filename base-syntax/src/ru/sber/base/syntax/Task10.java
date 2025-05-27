b2eba3c03dd99cce0631c069ef6b80a99fbfb7ed
hearwy <mipwqre@gmail.com>
2025-05-27 08:52:44 +0300

    Commit Message

---
+++ base-syntax/src/ru/sber/base/syntax/Task10.java
@@ -0,0 +1,8 @@
+package ru.sber.base.syntax;
+import javax.swing.JOptionPane;
+
+public class Task10{
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Привет, [твоё имя]");
+    }
+}
